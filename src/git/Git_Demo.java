0b3417b478ed93aee72c1b01d19d31c0625bf5da
pradnya2915 <126941969+pradnya2915@users.noreply.github.com>
2023-09-13 14:06:15 +0530

    first comments...

---
+++ src/git/Git_Demo.java
@@ -0,0 +1,21 @@
+package git;
+
+public class Git_Demo {
+
+	public void demo() {
+		String username="Admin@gmail.com";
+		String password="Admin@123";
+		
+		if(username.equals ("Admin@gmail.com")&& password.equals("Admin@123")){
+			System.out.println("login successfully");
+		}
+		
+	}
+	public static void main(String[] args) {
+		Git_Demo d=new Git_Demo();
+		d.demo();
+		
+
+	}
+
+}
